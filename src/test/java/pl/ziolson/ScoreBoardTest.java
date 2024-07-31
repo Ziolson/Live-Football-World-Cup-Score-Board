ba7ce7096edcf88e388437d1540082d0f1e5cc07
Mariusz Ziółek <ziolson92@gmail.com>
2024-07-31 22:09:50 +0200

    Add missing test for getSummary

--- src/test/java/pl/ziolson/ScoreBoardTest.java
+++ src/test/java/pl/ziolson/ScoreBoardTest.java
@@ -1,7 +1,8 @@
 package pl.ziolson;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -31,5 +32,20 @@ class ScoreBoardTest {
         assertEquals(1, scoreBoard.getSummary().size());
         scoreBoard.finishMatch("Mexico", "Canada");
         assertEquals(0, scoreBoard.getSummary().size());
+    }
+
+    @Test
+    void getSummary() {
+        ScoreBoard scoreBoard = new ScoreBoard();
+        scoreBoard.startMatch("Germany", "France");
+        scoreBoard.updateScore("Germany", "France", 2, 2);
+        scoreBoard.startMatch("Argentina", "Australia");
+        scoreBoard.updateScore("Argentina", "Australia", 3, 1);
+
+        List<Match> summary = scoreBoard.getSummary();
+
+        assertEquals(2, summary.size());
+        assertEquals("Argentina", summary.get(0).getHomeTeam());
+        assertEquals("Germany", summary.get(1).getHomeTeam());
     }
 }
