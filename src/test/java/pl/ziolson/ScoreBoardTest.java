276143daf2aef4f5b6c75ce7bdf7a543fe09d0cb
Mariusz Ziółek <ziolson92@gmail.com>
2024-07-31 23:00:31 +0200

    Add more test cases for ScoreBoardTest

--- src/test/java/pl/ziolson/ScoreBoardTest.java
+++ src/test/java/pl/ziolson/ScoreBoardTest.java
@@ -9,20 +9,68 @@ import static org.junit.jupiter.api.Assertions.*;
 class ScoreBoardTest {
 
     @Test
-    void startMatch() {
+    void startMatch_happyPath() {
         ScoreBoard scoreBoard = new ScoreBoard();
         scoreBoard.startMatch("Mexico", "Canada");
         assertEquals(1, scoreBoard.getSummary().size());
     }
 
     @Test
-    void updateScore() {
+    void startMatch_duplicateMatch() {
+        ScoreBoard scoreBoard = new ScoreBoard();
+        scoreBoard.startMatch("Mexico", "Canada");
+        scoreBoard.startMatch("Mexico", "Canada");
+        assertEquals(1, scoreBoard.getSummary().size());
+    }
+
+    @Test
+    void startMatch_nullHomeTeam() {
+        ScoreBoard scoreBoard = new ScoreBoard();
+        scoreBoard.startMatch(null, "Canada");
+        assertEquals(0, scoreBoard.getSummary().size());
+    }
+
+    @Test
+    void startMatch_nullAwayTeam() {
+        ScoreBoard scoreBoard = new ScoreBoard();
+        scoreBoard.startMatch("Mexico", null);
+        assertEquals(0, scoreBoard.getSummary().size());
+    }
+
+    @Test
+    void updateScore_happyPath() {
         ScoreBoard scoreBoard = new ScoreBoard();
         scoreBoard.startMatch("Mexico", "Canada");
         scoreBoard.updateScore("Mexico", "Canada", 1, 0);
         assertEquals(1, scoreBoard.getSummary().size());
         assertEquals(1, scoreBoard.getSummary().getFirst().getHomeScore());
         assertEquals(0, scoreBoard.getSummary().getFirst().getAwayScore());
+    }
+
+    @Test
+    void updateScore_nullHomeTeam() {
+        ScoreBoard scoreBoard = new ScoreBoard();
+        scoreBoard.startMatch("Mexico", "Canada");
+        Exception exception = assertThrows(Exception.class, () -> {
+            scoreBoard.updateScore(null, "Canada", 1, 0);
+        });
+    }
+
+    @Test
+    void updateScore_nullAwayTeam() {
+        ScoreBoard scoreBoard = new ScoreBoard();
+        scoreBoard.startMatch("Mexico", "Canada");
+        Exception exception = assertThrows(Exception.class, () -> {
+            scoreBoard.updateScore("Mexico", null, 1, 0);
+        });
+    }
+
+    @Test
+    void updateScore_matchNotStarted() {
+        ScoreBoard scoreBoard = new ScoreBoard();
+        Exception exception = assertThrows(Exception.class, () -> {
+            scoreBoard.updateScore("Mexico", "Canada", 1, 0);
+        });
     }
 
     @Test
@@ -35,7 +83,49 @@ class ScoreBoardTest {
     }
 
     @Test
-    void getSummary() {
+    void finishMatch_matchNotStarted() {
+        ScoreBoard scoreBoard = new ScoreBoard();
+        Exception exception = assertThrows(Exception.class, () -> {
+            scoreBoard.finishMatch("Mexico", "Canada");
+        });
+    }
+
+    @Test
+    void finishMatch_nullHomeTeam() {
+        ScoreBoard scoreBoard = new ScoreBoard();
+        scoreBoard.startMatch("Mexico", "Canada");
+        Exception exception = assertThrows(Exception.class, () -> {
+            scoreBoard.finishMatch(null, "Canada");
+        });
+    }
+
+    @Test
+    void finishMatch_nullAwayTeam() {
+        ScoreBoard scoreBoard = new ScoreBoard();
+        scoreBoard.startMatch("Mexico", "Canada");
+        assertEquals(1, scoreBoard.getSummary().size());
+        Exception exception = assertThrows(Exception.class, () -> {
+            scoreBoard.finishMatch("Mexico", null);
+        });
+    }
+
+    @Test
+    void getSummary_happyPath() {
+        ScoreBoard scoreBoard = new ScoreBoard();
+        scoreBoard.startMatch("Mexico", "Canada");
+        scoreBoard.updateScore("Mexico", "Canada", 0, 5);
+        scoreBoard.startMatch("Spain", "Brazil");
+        scoreBoard.updateScore("Spain", "Brazil", 10, 2);
+
+        List<Match> summary = scoreBoard.getSummary();
+
+        assertEquals(2, summary.size());
+        assertEquals("Spain", summary.get(0).getHomeTeam());
+        assertEquals("Mexico", summary.get(1).getHomeTeam());
+    }
+
+    @Test
+    void getSummary_sameScore() {
         ScoreBoard scoreBoard = new ScoreBoard();
         scoreBoard.startMatch("Germany", "France");
         scoreBoard.updateScore("Germany", "France", 2, 2);
